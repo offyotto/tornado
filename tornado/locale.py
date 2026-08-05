70028bef21499b2d48ffa188e19b4e53d4cd6077
offyotto <nazishamin65@gmail.com>
2026-08-05 08:39:41 -0400

    Fall back to full format for future dates

--- tornado/locale.py
+++ tornado/locale.py
@@ -355,7 +355,7 @@ class Locale:
             date = date.replace(tzinfo=datetime.timezone.utc)
         now = datetime.datetime.now(datetime.timezone.utc)
         if date > now:
-            if relative and (date - now).seconds < 60:
+            if relative and (date - now).total_seconds() < 60:
                 # Due to click skew, things are some things slightly
                 # in the future. Round timestamps in the immediate
                 # future down to now in relative mode.
