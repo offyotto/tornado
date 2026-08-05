6d33e0c250b3a7402d2d6ebbd98d8691429a1442
offyotto <nazishamin65@gmail.com>
2026-08-05 08:30:28 -0400

    Fall back to full format for future dates

--- tornado/locale.py
+++ tornado/locale.py
@@ -355,7 +355,7 @@ class Locale:
             date = date.replace(tzinfo=datetime.timezone.utc)
         now = datetime.datetime.now(datetime.timezone.utc)
         if date > now:
-            if relative and (date - now).seconds < 60:
+            if relative and (date - now).total_seconds() < 60:
                 # Due to click skew, things are some things slightly
                 # in the future. Round timestamps in the immediate
                 # future down to now in relative mode.
@@ -413,7 +413,7 @@ class Locale:
             str_time = "%d:%02d" % (local_date.hour, local_date.minute)
         elif self.code == "zh_CN":
             str_time = "%s%d:%02d" % (
-                ("\u4e0a\u5348", "\u4e0b\u5348")[local_date.hour >= 12],
+                ("\\u4e0a\\u5348", "\\u4e0b\\u5348")[local_date.hour >= 12],
                 local_date.hour % 12 or 12,
                 local_date.minute,
             )
@@ -465,7 +465,7 @@ class Locale:
             return ""
         if len(parts) == 1:
             return parts[0]
-        comma = " \u0648 " if self.code.startswith("fa") else ", "
+        comma = " \\u0648 " if self.code.startswith("fa") else ", "
         return _("%(commas)s and %(last)s") % {
             "commas": comma.join(parts[:-1]),
             "last": parts[len(parts) - 1],
--- tornado/test/locale_test.py
+++ tornado/test/locale_test.py
@@ -33,7 +33,7 @@ class TranslationLoaderTest(unittest.TestCase):
         )
         locale = tornado.locale.get("fr_FR")
         self.assertTrue(isinstance(locale, tornado.locale.CSVLocale))
-        self.assertEqual(locale.translate("school"), "\u00e9cole")
+        self.assertEqual(locale.translate("school"), "\\u00e9cole")
 
     def test_csv_bom(self):
         with open(
@@ -53,7 +53,7 @@ class TranslationLoaderTest(unittest.TestCase):
                 tornado.locale.load_translations(tmpdir)
                 locale = tornado.locale.get("fr_FR")
                 self.assertIsInstance(locale, tornado.locale.CSVLocale)
-                self.assertEqual(locale.translate("school"), "\u00e9cole")
+                self.assertEqual(locale.translate("school"), "\\u00e9cole")
             finally:
                 shutil.rmtree(tmpdir)
 
@@ -64,23 +64,23 @@ class TranslationLoaderTest(unittest.TestCase):
         )
         locale = tornado.locale.get("fr_FR")
         self.assertTrue(isinstance(locale, tornado.locale.GettextLocale))
-        self.assertEqual(locale.translate("school"), "\u00e9cole")
+        self.assertEqual(locale.translate("school"), "\\u00e9cole")
         self.assertEqual(locale.pgettext("law", "right"), "le droit")
         self.assertEqual(locale.pgettext("good", "right"), "le bien")
         self.assertEqual(locale.pgettext("organization", "club", "clubs", 1), "le club")
         self.assertEqual(
             locale.pgettext("organization", "club", "clubs", 2), "les clubs"
         )
-        self.assertEqual(locale.pgettext("stick", "club", "clubs", 1), "le b\xe2ton")
-        self.assertEqual(locale.pgettext("stick", "club", "clubs", 2), "les b\xe2tons")
+        self.assertEqual(locale.pgettext("stick", "club", "clubs", 1), "le b\\xe2ton")
+        self.assertEqual(locale.pgettext("stick", "club", "clubs", 2), "les b\\xe2tons")
 
 
 class LocaleDataTest(unittest.TestCase):
     def test_non_ascii_name(self):
         name = tornado.locale.LOCALE_NAMES["es_LA"]["name"]
         self.assertTrue(isinstance(name, unicode_type))
-        self.assertEqual(name, "Espa\u00f1ol")
-        self.assertEqual(utf8(name), b"Espa\xc3\xb1ol")
+        self.assertEqual(name, "Espa\\u00f1ol")
+        self.assertEqual(utf8(name), b"Espa\\xc3\\xb1ol")
 
 
 class EnglishTest(unittest.TestCase):
@@ -136,6 +136,12 @@ class EnglishTest(unittest.TestCase):
                 )
 
                 date = now - datetime.timedelta(days=500)
+                self.assertEqual(
+                    locale.format_date(date, full_format=False, shorter=True),
+                    "%s %d, %d" % (locale._months[date.month - 1], date.day, date.year),
+                )
+
+                date = now + datetime.timedelta(days=1, seconds=30)
                 self.assertEqual(
                     locale.format_date(date, full_format=False, shorter=True),
                     "%s %d, %d" % (locale._months[date.month - 1], date.day, date.year),
